fa0f109d59034557a0b9b471264153dc965a9b98
underscoreanuj <underscoreanuj@gmail.com>
2020-01-08 16:30:45 +0530

    register user added

---
+++ src/resolvers.ts
@@ -0,0 +1,23 @@
+import { ResolverMap } from './types/gql-utils';
+import * as bcrypt from 'bcryptjs';
+import { User } from './entity/User';
+
+
+export const resolvers: ResolverMap = {
+    Query: {
+        hello: (_, { name }: GQL.IHelloOnQueryArguments) => `Hello ${name || 'World'}`
+    },
+    Mutation: {
+        register: async(_, { email, password }: GQL.IRegisterOnMutationArguments) => {
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const new_user = User.create({
+                email,
+                password: hashedPassword
+            });
+
+            await new_user.save();
+
+            return true;
+        }
+    }
+};
